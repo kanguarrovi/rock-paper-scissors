557df40404845fe3c9dbbcca432c8aff02badd10
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2017-11-27 14:32:28 -0600

    Source code init

---
+++ rps.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""rps.py: A very simple Rock-paper-scissors python console game."""
+
+__author__      = "Carlos Arroyo Villalobos a.k.a Kangu"
+__copyright__   = "Copyleft 2017, Heredia, Costa Rica"
+
+
